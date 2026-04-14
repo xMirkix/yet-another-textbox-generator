7e96b851621a48346895ea856216ca4426c8974d
Michael Andert <michael.andert@htlstp.at>
2026-04-14 19:24:49 +0200

    added ui file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
+from PySide6.QtCore import QFile
 from PySide6.QtWidgets import QApplication, QMainWindow
-
+from PySide6.QtUiTools import QUiLoader
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -14,8 +15,14 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     app = QApplication([])
-    window = QMainWindow()
+
+    ui_file = QFile("ui.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
     window.show()
+
     app.exec()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
