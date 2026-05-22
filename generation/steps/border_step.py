7f5f39c38fc1462614e9372c0bc6526856aaf595
michael <michael.andert@htlpst.at>
2026-05-22 14:13:39 +0200

    border step done

--- generation/steps/border_step.py
+++ generation/steps/border_step.py
@@ -34,10 +34,11 @@ def apply_deltarune(settings: BorderSettings) -> Image.Image:
     border_source = Image.open(border_path).convert("RGBA")
 
     width, height = 297, 84
-    image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    result = Image.new("RGBA", (width, height), (0, 0, 0, 0))
 
 
-    r, g, b = settings.color.r, settings.color.g, settings.color.b
+    r, g, b = get_color_with_least_values(settings.color.r, settings.color.g, settings.color.b)
+
 
     colored_border = Image.new("RGBA", border_source.size, (0, 0, 0, 0))
     pixels_src = border_source.load()
@@ -45,20 +46,33 @@ def apply_deltarune(settings: BorderSettings) -> Image.Image:
 
     for x in range(border_source.width):
         for y in range(border_source.height):
-            pixel_src_r, pixel_src_g, pixel_src_b, pixel_src_alpha = pixels_src[x, y]
-            if pixel_src_alpha > 0:
-                brightness = (pixel_src_r + pixel_src_g + pixel_src_b) / (3 * 255)
-                pixels_dst[x, y] = (
-                    int(r * brightness),
-                    int(g * brightness),
-                    int(b * brightness),
-                    pixel_src_alpha
-                )
+            for_each_pixel(x, y, r, g, b, pixels_src, pixels_dst)
 
     pixels_dst[6, 6] = (170, 255, 230, 255)
     pixels_dst[290, 6] = (170, 255, 230, 255)
     pixels_dst[6, 77] = (170, 255, 230, 255)
     pixels_dst[290, 77] = (170, 255, 230, 255)
 
-    image.paste(colored_border, (0, 0), colored_border)
-    return image
+    result.paste(colored_border, (0, 0), colored_border)
+    return result
+
+def for_each_pixel(x: int, y: int, r: int, g: int, b: int, pixels_src, pixels_dst):
+    pixel_src_r, pixel_src_g, pixel_src_b, pixel_src_alpha = pixels_src[x, y]
+
+    if pixel_src_alpha > 0:
+        brightness = (pixel_src_r + pixel_src_g + pixel_src_b) / (3 * 255)
+        final_r = int(r * brightness) % 256
+        final_g = int(g * brightness) % 256
+        final_b = int(b * brightness) % 256
+
+        pixels_dst[x, y] = (
+            final_r,
+            final_g,
+            final_b,
+            pixel_src_alpha
+            )
+
+def get_color_with_least_values(r: int, g: int, b: int) -> tuple[int, int, int]:
+    if r + g + b < 50:
+        return max(r, 20), max(g, 20), max(b, 20)
+    return r, g, b
